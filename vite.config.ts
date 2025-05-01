45eb0dcf8f06ba3c25a9be90a8936c73b63cf9de
Lee MinGi <mingi3442@gmail.com>
2025-05-02 00:08:28 +0900

    🔧 Udpate config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
 import path from "path"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-2/" : "",
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-3/" : "",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
